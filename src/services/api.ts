929a544af1fddcc29d44df8eace09d6e6eaba58a
Jamil Filho <jamilf.alien@gmail.com>
2022-09-27 16:15:34 -0300

    chore: inicializando o repositorio

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const api = axios.create({
     //baseURL: 'http://localhost:3333/'
-    baseURL: 'https://podcastrstrapi.herokuapp.com/' //Strapi
+    baseURL: 'https://cadamanha-adm.herokuapp.com/' //Strapi
 
 });
